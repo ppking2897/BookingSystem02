5d768034695284d9981a303e2a5bf47d887ce2cc
ppking <ppking2897@gmail.com>
2016-12-21 17:45:23 +0800

    123

--- app/src/main/java/com/example/user/ordersystem/MainActivity.java
+++ app/src/main/java/com/example/user/ordersystem/MainActivity.java
@@ -10,17 +10,36 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
         {
+            //---------------FB---------------------
+            private LoginButton loginButton;
+            private CallbackManager callbackManager;
+            LoginManager lmg;
+            private ProfileTracker profileTracker;
+            private String loginState , logout = "Log out", logout2 = "登出" , loginID;
+            private int count;
 
 
     @Override
@@ -55,7 +74,76 @@ public class MainActivity extends AppCompatActivity
 
         Button button = (Button)findViewById(R.id.login);
         button.setOnClickListener(clickListener);
+
+
+        //-----------------FB--------------------
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+
+
+        lmg.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                Log.v("ppking" , "oldProfile : "+ oldProfile );
+                Log.v("ppking" , "currentProfile : "+ currentProfile );
+
+                if(currentProfile !=null) {
+                    loginID = currentProfile.getId();
+                    //TODO : loginID 是由FB讀取出來判斷帳戶的依據並儲存於資料庫內
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            try {
+                                count++;
+
+                                MultipartUtility mu = new MultipartUtility("https://android-test-db-ppking2897.c9users.io/DataBase/Insert02.php", "UTF-8");
+                                if(count<2) {
+                                    mu.addFormField("accountId", loginID);
+                                    mu.addFormField("seatIdNumber", "0");
+                                    mu.addFormField("checkOut", "0");
+                                    List<String> ret = mu.finish();
+                                    Log.v("ppking","profileTracker" + loginID);
+                                    Intent it = new Intent(MainActivity.this,SeatPage.class);
+                                    it.putExtra("ppking" ,loginID);
+                                }
+                            } catch (Exception e) {
+                                Log.v("ppking", "DB Error:" + e.toString());
+                            }
+                        }
+                    }.start();
+                }
+            }
+
+        };
+        //-----------------end-----------------------------------
     }
+    //-------------FB-------------------------
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        //Log.v("ppking" , "onActivityResult");
+    }
+    //--------------end---------------------------
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/example/user/ordersystem/SeatPage.java
+++ app/src/main/java/com/example/user/ordersystem/SeatPage.java
@@ -4,15 +4,22 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SeatPage extends AppCompatActivity {
     Button[] btns = new Button[10];
@@ -26,6 +33,12 @@ public class SeatPage extends AppCompatActivity {
     private TextView tableNmb;
     private String tableNumber;
 
+    //----------------------------------
+    private int count;
+    private UIHandler uiHandler;
+    private String SeatState1 , SeatState2 , SeatState3 , SeatState4 , SeatState5 , SeatState6,
+                   SeatState7 , SeatState8 , SeatState9 , SeatState0;
+    private String loginId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,8 +69,18 @@ public class SeatPage extends AppCompatActivity {
             btns[i].setOnClickListener(clickListener);}
 
         //從資料庫提取資料
+        uiHandler = new UIHandler();
+        Intent it = getIntent();
+        loginId = it.getStringExtra("ppking");
+        Log.v("ppking" , "loginidseat : " + loginId);
+        pushData();
 
     }
+
+
+
+
+
 //找View By ID
     public void doFindView(){
 
@@ -113,6 +136,7 @@ public class SeatPage extends AppCompatActivity {
         public void onClick(View v){
             Intent it = new Intent();
             it.setClass(SeatPage.this,SuccessPage.class);
+            pushData();
             startActivity(it);
             finish();
 
@@ -123,4 +147,107 @@ public class SeatPage extends AppCompatActivity {
 
     //
     //桌號資料變數> tableNumber
+
+
+
+    //提取資料
+    public void pullData(){
+
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+
+                    MultipartUtility mu = new MultipartUtility("https://android-test-db-ppking2897.c9users.io/DataBase/SeatQuery02.php", "UTF-8");
+                    List<String> ret = mu.finish();
+
+                    parseJSON(ret.toString());
+
+
+                } catch (Exception e) {
+                    Log.v("ppking", "DB Error:" + e.toString());
+                }
+            }
+        }.start();
+    }
+    public void pushData(){
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    count++;
+
+                    MultipartUtility mu = new MultipartUtility("https://android-test-db-ppking2897.c9users.io/DataBase/AccountUpload02.php", "UTF-8");
+                    if(count<2) {
+                        mu.addFormField("accountId", loginId);
+                        mu.addFormField("resdate", "0");
+                        mu.addFormField("restime", "0");
+                        List<String> ret = mu.finish();
+                        Log.v("ppking", "ret update:: " + ret);
+                    }
+                } catch (Exception e) {
+                    Log.v("ppking", "DB Error:" + e.toString());
+                }
+            }
+        }.start();
+    }
+
+
+
+    //接收JSON格式並且找特定字串
+    private void parseJSON(String json){
+        LinkedList accountInfo = new LinkedList<>();
+        try{
+
+//            JSONObject jsonObject = new JSONArray(json).getJSONObject(0);
+            JSONArray jsonArray = new JSONArray(json).getJSONArray(0);
+            for(int i = 0 ; i <=9 ; i++ ) {
+                String stringNo = jsonArray.getString(i);
+                accountInfo.add(stringNo);
+//                String stringNo1 = jsonArray.getString(1);
+//                accountInfo.add(stringNo1);
+            }
+
+            Message mesg = new Message();
+            Bundle data = new Bundle();
+            for (int i = 0 ; i<=9 ; i++) {
+                data.putCharSequence("data"+i, accountInfo.get(i).toString());
+//            data.putCharSequence("data1", accountInfo.get(1).toString());
+//            data.putCharSequence("data2",accountInfo.get(2).toString());
+            }
+            mesg.setData(data);
+            mesg.what=0;
+            uiHandler.sendMessage(mesg);
+
+
+        }catch (Exception e){
+            Log.v("ppking", "Error : " + e.toString());
+        }
+    }
+    //由handler這邊將資料丟到前景UI
+    private class UIHandler extends android.os.Handler{
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 0 :
+//                    textView.setText("Accound : "+msg.getData().getCharSequence("data0")+"\n");
+//                    textView.append("SeatIdNumber : "+msg.getData().getCharSequence("data1")+"\n");
+//                    textView.append("Checkout : $"+msg.getData().getCharSequence("data2")+"\n");
+                    //TODO:各座位的狀態，去判斷座位
+                    SeatState0 = msg.getData().getCharSequence("data0").toString();
+                    SeatState1 = msg.getData().getCharSequence("data1").toString();
+                    SeatState2 = msg.getData().getCharSequence("data2").toString();
+                    SeatState3 = msg.getData().getCharSequence("data3").toString();
+                    SeatState4 = msg.getData().getCharSequence("data4").toString();
+                    SeatState5 = msg.getData().getCharSequence("data5").toString();
+                    SeatState6 = msg.getData().getCharSequence("data6").toString();
+                    SeatState7 = msg.getData().getCharSequence("data7").toString();
+                    SeatState8 = msg.getData().getCharSequence("data8").toString();
+                    SeatState9 = msg.getData().getCharSequence("data9").toString();
+                    break;
+            }
+        }
+    }
 }
